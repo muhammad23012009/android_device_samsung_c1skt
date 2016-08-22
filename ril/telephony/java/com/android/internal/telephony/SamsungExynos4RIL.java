5bf9508d3add4db7fc485e548ff19fd252e70c1a
fullgreen <abin030187@naver.com>
2016-08-23 07:21:31 +0900

    SamsungExynos4RIL.java : Stabilizing signal
    Signed-off-by: fullgreen <abin030187@naver.com>

--- ril/telephony/java/com/android/internal/telephony/SamsungExynos4RIL.java
+++ ril/telephony/java/com/android/internal/telephony/SamsungExynos4RIL.java
@@ -133,28 +133,6 @@ public class SamsungExynos4RIL extends RIL implements CommandsInterface {
         return cardStatus;
     }
 
-
-    @Override
-    protected Object responseSignalStrength(Parcel p) {
-        int numInts = 12;
-        int response[];
-
-        // Get raw data
-        response = new int[numInts];
-        for (int i = 0; i < numInts; i++) {
-            response[i] = p.readInt();
-        }
-        //gsm
-        response[0] &= 0xff;
-        //cdma
-        response[2] %= 256;
-        response[4] %= 256;
-        response[7] &= 0xff;
-
-        return new SignalStrength(response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7], response[8], response[9], response[10], response[11], true);
-
-    }
-
     @Override
     public void setPhoneType(int phoneType){
         super.setPhoneType(phoneType);
