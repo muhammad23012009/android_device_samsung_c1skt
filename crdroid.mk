faf82ac2a23d155163bf309269dadface67c8724
fullgreen <abin030187@naver.com>
2016-09-16 13:00:22 +0900

    c1skt: crdroid bringup
    Signed-off-by: fullgreen <abin030187@naver.com>

---
+++ crdroid.mk
@@ -0,0 +1,30 @@
+# Release name
+PRODUCT_RELEASE_NAME := c1skt
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/c1skt/c1skt.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common crdroid stuff.
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
+$(call inherit-product, vendor/crdroid/config/nfc_enhanced.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := c1skt
+PRODUCT_NAME := crdroid_c1skt
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SHV-E210S
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=c1skt \
+    TARGET_DEVICE=c1skt \
+    BUILD_FINGERPRINT="samsung/c1skt/c1skt:4.4.4/KTU84P/E210SKSUKOL2:user/release-keys" \
+    PRIVATE_BUILD_DESC="c1skt-user 4.4.4 KTU84P E210SKSUKOL2 release-keys"
