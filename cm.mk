ea3c5150a28958ff1033b8ab824936ce570c8954
fullgreen <abin030187@naver.com>
2016-11-12 15:04:44 +0900

    Initial commit of c1skt
    --------------------------------------------------------
    c1skt : Change to build fingerprint / ID / Product Name
    --------------------------------------------------------
    c1skt: Change the bluetooth name
    --------------------------------------------------------
    
    Signed-off-by: fullgreen <abin030187@naver.com>

--- cm.mk
+++ cm.mk
@@ -1,9 +1,9 @@
 # Release name
-PRODUCT_RELEASE_NAME := i9300
+PRODUCT_RELEASE_NAME := c1skt
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/smdk4412-common/common.mk)
-$(call inherit-product, device/samsung/i9300/i9300.mk)
+$(call inherit-product, device/samsung/c1skt/c1skt.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
@@ -16,15 +16,15 @@ TARGET_SCREEN_WIDTH := 720
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := i9300
-PRODUCT_NAME := cm_i9300
+PRODUCT_DEVICE := c1skt
+PRODUCT_NAME := cm_c1skt
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-I9300
+PRODUCT_MODEL := SHV-E210S
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-		PRODUCT_NAME=m0xx \
-		TARGET_DEVICE=m0 \
-		BUILD_FINGERPRINT="samsung/m0xx/m0:4.3/JSS15J/I9300XXUGMJ9:user/release-keys" \
-		PRIVATE_BUILD_DESC="m0xx-user 4.3 JSS15J I9300XXUGMJ9 release-keys"
+		PRODUCT_NAME=c1skt \
+		TARGET_DEVICE=c1skt \
+		BUILD_FINGERPRINT="samsung/c1skt/c1skt:4.4.4/KTU84P/E210SKSUKOL2:user/release-keys" \
+		PRIVATE_BUILD_DESC="c1skt-user 4.4.4 KTU84P E210SKSUKOL2 release-keys"
