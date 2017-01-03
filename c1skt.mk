b84541bbf98fcbac6bf95db5528fc45553305445
fullgreen <abin030187@naver.com>
2017-01-03 14:19:31 +0900

    Revert "c1skt: add Operators"
    This reverts commit 04e339177c1ed964fc3edb2ef15611193b61be37.

--- c1skt.mk
+++ c1skt.mk
@@ -110,9 +110,4 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
-
-# SPN override
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
-
 $(call inherit-product-if-exists, vendor/samsung/c1skt/c1skt-vendor.mk)
